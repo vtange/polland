b66a6a2f2eba41a83306aca8df8636a9a0faa3b3
Victor Tang <victortang1992@gmail.com>
2016-01-23 13:01:16 -0800

    use passports boilerplate

---
+++ config/database.js
@@ -0,0 +1,7 @@
+console.log("	CONFIG/DATABASE.JS")
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://localhost/passports-media' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
