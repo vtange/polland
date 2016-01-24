09f42bdbd2491d13466357052f5aa1e7fc899dde
Victor Tang <victortang1992@gmail.com>
2016-01-23 18:51:59 -0800

    throw in old polland css

--- public/app.js
+++ public/app.js
@@ -1,9 +1,52 @@
 (function() {
     //start of function
-  var app = angular.module('polland', []);
+  var app = angular.module('VoteApp', []);
 
-app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
+app.factory('UserService', function(){
 
+});//end of service
+
+app.controller('MainCtrl', ['$scope', 'UserService', function($scope, UserService){
+    $scope.UserService = UserService; // load service
+       //init
+    $scope.list = [];
+    $scope.LoginDetails = {};
+    $scope.activeUser = null;
+    $scope.show_login = function () {
+        if ($scope.activeUser) {
+            return { "transform": "translateX(300%)" };
+        }
+        else{
+        return { "transform": "translateX(0%)" };
+        }
+    };
+    $scope.show_user = function () {
+        if ($scope.activeUser) {
+            return { "transform": "translateX(0%)" };
+        }
+        else{
+        return { "transform": "translateX(-200%)" };
+        }
+    };
+    
+    $scope.registerMode = "Login";
+    $scope.toggleRegisterTxt = "New? Register Here!";
+    $scope.toggleRegister = function(){
+        if ($scope.registerMode == "Login"){
+            $scope.registerMode = "Register";
+            $scope.toggleRegisterTxt = "Back to Login";
+        }
+        else{
+            $scope.registerMode = "Login";
+            $scope.toggleRegisterTxt = "New? Register Here!";
+        }
+    };
+    $scope.addUser = function(){
+
+    };
+    $scope.userLogout = function(){
+
+    };
 }]);//end of controller
   //end of function
 })();
