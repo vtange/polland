c7a97eafd96cecf98b160e65de00a423e9a3f9b1
Victor Tang <victortang1992@gmail.com>
2016-01-23 18:21:09 -0800

    client side scripts and styles

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ console.log("	CONFIG/DATABASE.JS")
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passports-media' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://localhost/polland' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
---
+++ public/app.js
@@ -0,0 +1,9 @@
+(function() {
+    //start of function
+  var app = angular.module('polland', []);
+
+app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
+
+}]);//end of controller
+  //end of function
+})();
