19d00d81f5e7b9b94a2a61c9f36abc18308e5750
Victor Tang <victortang1992@gmail.com>
2016-01-23 19:01:21 -0800

    split single file app to multiple files

--- public/app.js
+++ public/app.js
@@ -3,50 +3,15 @@
   var app = angular.module('VoteApp', []);
 
 app.factory('UserService', function(){
-
+	var obj = {};
+	return obj;
 });//end of service
 
 app.controller('MainCtrl', ['$scope', 'UserService', function($scope, UserService){
     $scope.UserService = UserService; // load service
        //init
-    $scope.list = [];
-    $scope.LoginDetails = {};
     $scope.activeUser = null;
-    $scope.show_login = function () {
-        if ($scope.activeUser) {
-            return { "transform": "translateX(300%)" };
-        }
-        else{
-        return { "transform": "translateX(0%)" };
-        }
-    };
-    $scope.show_user = function () {
-        if ($scope.activeUser) {
-            return { "transform": "translateX(0%)" };
-        }
-        else{
-        return { "transform": "translateX(-200%)" };
-        }
-    };
-    
-    $scope.registerMode = "Login";
-    $scope.toggleRegisterTxt = "New? Register Here!";
-    $scope.toggleRegister = function(){
-        if ($scope.registerMode == "Login"){
-            $scope.registerMode = "Register";
-            $scope.toggleRegisterTxt = "Back to Login";
-        }
-        else{
-            $scope.registerMode = "Login";
-            $scope.toggleRegisterTxt = "New? Register Here!";
-        }
-    };
-    $scope.addUser = function(){
 
-    };
-    $scope.userLogout = function(){
-
-    };
 }]);//end of controller
   //end of function
 })();
