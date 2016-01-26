1e5158117156d100c16aa9d30f85924cd271db25
Victor Tang <victortang1992@gmail.com>
2016-01-25 19:39:45 -0800

    render user email on logged in users

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,9 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
+        res.render('index.ejs', {
+            user : req.user // get the user out of session and pass to template
+        }); // load the index.ejs file
     });
 
     // =====================================
