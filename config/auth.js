69f29b71b6fb93fda3b73da3aff1d94af5a2961b
Victor Tang <victortang1992@gmail.com>
2016-01-26 22:08:29 -0800

    login/register with fb

---
+++ config/auth.js
@@ -0,0 +1,23 @@
+console.log("	CONFIG/AUTH.JS")
+// config/auth.js
+module.exports = {
+	
+    'facebookAuth' : {
+        'clientID'      : '', // your App ID
+        'clientSecret'  : '', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'	//this goes in Facebook's "App URL" textbox
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+	
+};
