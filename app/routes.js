40b1fed38896013db09abf701a16412e176c4a9e
Victor Tang <victortang1992@gmail.com>
2016-01-26 18:04:07 -0800

    fix resetPasswordToken not being applied (part 3)

--- app/routes.js
+++ app/routes.js
@@ -103,7 +103,6 @@ module.exports = function(app, passport) {
     // FORGOT PASSWORD =====================
     // =====================================
 	app.post('/forgot', function(req, res, next) {
-		console.log("performing forgot action");
 	  asyncc.waterfall([
 		function(done) {
 		  crypto.randomBytes(20, function(err, buf) {
@@ -118,8 +117,8 @@ module.exports = function(app, passport) {
 			  return res.redirect('/forgot');
 			}
 
-			user.resetPasswordToken = token;
-			user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+			user.local.resetPasswordToken = token;
+			user.local.resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
 			user.save(function(err) {
 			  done(err, token, user);
@@ -154,28 +153,30 @@ module.exports = function(app, passport) {
 	  });
 	});
 	app.get('/reset/:token', function(req, res) {
-	  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+	  User.findOne({ 'local.resetPasswordToken': req.params.token, 'local.resetPasswordExpires': { $gt: Date.now() } }, function(err, user) {
 		if (!user) {
 		  req.flash('error', 'Password reset token is invalid or has expired.');
 		  return res.redirect('/forgot');
 		}
 		res.render('reset.ejs', {
+		  message: req.flash('info'),
 		  user: req.user
 		});
 	  });
 	});
+	
 	app.post('/reset/:token', function(req, res) {
 	  asyncc.waterfall([
 		function(done) {
-		  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+		  User.findOne({ 'local.resetPasswordToken': req.params.token, 'local.resetPasswordExpires': { $gt: Date.now() } }, function(err, user) {
 			if (!user) {
-			  req.flash('error', 'Password reset token is invalid or has expired.');
+			  req.flash('info', 'Password reset token is invalid or has expired.');
 			  return res.redirect('back');
 			}
 
-			user.password = req.body.password;
-			user.resetPasswordToken = undefined;
-			user.resetPasswordExpires = undefined;
+			user.local.password = req.body.password;
+			user.local.resetPasswordToken = undefined;
+			user.local.resetPasswordExpires = undefined;
 
 			user.save(function(err) {
 			  req.logIn(user, function(err) {
@@ -200,7 +201,7 @@ module.exports = function(app, passport) {
 			  'This is a confirmation that the password for your account ' + user.local.email + ' has just been changed.\n'
 		  };
 		  transporter.sendMail(mailOptions, function(err) {
-			req.flash('success', 'Success! Your password has been changed.');
+			req.flash('info', 'Success! Your password has been changed.');
 			done(err);
 		  });
 		}
