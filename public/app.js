26948bef28006ec2ba8c8586302f84d4d91e4943
Victor Tang <victortang1992@gmail.com>
2016-01-29 02:38:39 -0800

    mock "myPolls"

--- public/app.js
+++ public/app.js
@@ -9,7 +9,29 @@ app.factory('UserService', function(){
 
 app.controller('MainCtrl', ['$scope', 'UserService', function($scope, UserService){
     $scope.UserService = UserService; // load service
+	
     //$scope.otherPolls = [poll1, poll2]
+    $scope.myPolls = [{
+						question: 'Is Dart awesome?',
+						asker        : 'Ford',
+						postDate	: 'February 4, 2016',
+						choices: [
+						  { choice: 'Of course!', votes: 0 },
+						  { choice: 'Eh', votes: 0 },
+						  { choice: 'No. I like plain JS', votes: 0 }
+						]
+					  },
+					  {
+						question: 'Is it OK to eat ice cream when you are sick?',
+						asker        : 'Thomas',
+						postDate	: 'July 31, 2015',
+						choices: [
+						  { choice: '100% no', votes: 0 },
+						  { choice: '200% no', votes: 0 },
+						  { choice: '300% no', votes: 0 }
+						]
+					  }]
+	
     $scope.activeUser = null;
 	$scope.init = function(package) {
 		$scope.activeUser = JSON.parse(package)[0];
