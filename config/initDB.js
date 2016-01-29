f4e7fb5439b84771138c503d3a9968e133a69018
Victor Tang <victortang1992@gmail.com>
2016-01-28 17:40:35 -0800

    initDB is async.

--- config/initDB.js
+++ config/initDB.js
@@ -1,38 +1,52 @@
 console.log("	CONFIG/INITDB.JS");
 
 // load up the poll model
-var Polls       = require('../app/models/poll');
+var Poll       = require('../app/models/poll');
 
 module.exports = function(){
-				if (Polls.find().count() === 0) {
-				// create sample polls
-				var samplePolls = [
-				  {
-					question: 'Is Meteor awesome?',
-			        asker        : 'Anonymous',
-					postDate	: 'December 31, 2015',
-					choices: [
-					  { choice: 'Of course!', votes: 0 },
-					  { choice: 'Eh', votes: 0 },
-					  { choice: 'No. I like plain JS', votes: 0 }
-					]
-				  },
-				  {
-					question: 'Is CSS3 Flexbox the greatest thing since array_slice(bread)?',
-				   	asker        : 'Anonymous',
-					postDate	: 'December 31, 2015',
-					choices: [
-					  { choice: '100% yes', votes: 0 },
-					  { choice: '200% yes', votes: 0 },
-					  { choice: '300% yes', votes: 0 }
-					]
+				Poll.findOne(function(err, poll){
+
+					if (poll === null) {
+						console.log('create sample polls');
+					// create sample polls
+					var samplePolls = [
+					  {
+						question: 'Is Meteor awesome?',
+						asker        : 'Anonymous',
+						postDate	: 'December 31, 2015',
+						choices: [
+						  { choice: 'Of course!', votes: 0 },
+						  { choice: 'Eh', votes: 0 },
+						  { choice: 'No. I like plain JS', votes: 0 }
+						]
+					  },
+					  {
+						question: 'Is CSS3 Flexbox the greatest thing since array_slice(bread)?',
+						asker        : 'Anonymous',
+						postDate	: 'December 31, 2015',
+						choices: [
+						  { choice: '100% yes', votes: 0 },
+						  { choice: '200% yes', votes: 0 },
+						  { choice: '300% yes', votes: 0 }
+						]
+					  }
+					];
+
+					// loop over each sample poll and insert into database
+					for(var i = 0; i<samplePolls.length;i++){
+						var newPoll            = new Poll();
+						newPoll.question = samplePolls[i].question;
+						newPoll.asker = samplePolls[i].asker;
+						newPoll.postDate = samplePolls[i].postDate;
+						newPoll.choices = samplePolls[i].choices;
+						newPoll.save(function(err) {
+							if (err)
+								throw err;
+							console.log('saved a new poll')
+						});
+					};
+
 				  }
-				];
-
-				// loop over each sample poll and insert into database
-				samplePolls.forEach(function(poll) {
-				  Polls.insert(poll);
-				});
-
-			  }
+					
+			  });
 }
