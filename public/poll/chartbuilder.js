051f40c98ab772ad083ec7eb74579bd344d5e2f3
Victor Tang <victortang1992@gmail.com>
2016-01-29 18:23:29 -0800

    fix getElementbyId is null

--- public/poll/chartbuilder.js
+++ public/poll/chartbuilder.js
@@ -1,4 +1,4 @@
-(function(){
+document.addEventListener("DOMContentLoaded", function() {
 //Data
 	var pieData = [
    {
@@ -25,9 +25,10 @@
 	
 	
 // Get the context of the canvas element we want to select
+
 var context = document.getElementById('skills').getContext('2d');
 var skillsChart = new Chart(context).Pie(pieData);
 	
 	
 	
-})()
+});
