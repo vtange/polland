7f78ce3719c5c0f36b5c6599297de799ee421d5f
Victor Tang <victortang1992@gmail.com>
2016-01-30 01:17:15 -0800

    charts work and don't overlap

--- public/poll/chartbuilder.js
+++ public/poll/chartbuilder.js
@@ -26,64 +26,69 @@ app.controller('MainCtrl', ['$scope', function($scope){
 		  color: '#6AE128'
 	   }
 	];
-	var barData = [
-	   {
-		  value: 25,
-		  label: 'Java',
-		  color: '#811BD6'
-	   },
-	   {
-		  value: 10,
-		  label: 'Scala',
-		  color: '#9CBABA'
-	   },
-	   {
-		  value: 30,
-		  label: 'PHP',
-		  color: '#D18177'
-	   },
-	   {
-		  value : 35,
-		  label: 'HTML',
-		  color: '#6AE128'
-	   }
-	];
-	var lineData = [
-	   {
-		  value: 25,
-		  label: 'Java',
-		  color: '#811BD6'
-	   },
-	   {
-		  value: 10,
-		  label: 'Scala',
-		  color: '#9CBABA'
-	   },
-	   {
-		  value: 30,
-		  label: 'PHP',
-		  color: '#D18177'
-	   },
-	   {
-		  value : 35,
-		  label: 'HTML',
-		  color: '#6AE128'
-	   }
-	];
+	var barData = {
+		labels: ["January", "February", "March", "April", "May", "June", "July"],
+			datasets: [
+				{
+					label: "My First dataset",
+					fillColor: "rgba(220,220,220,0.5)",
+					strokeColor: "rgba(220,220,220,0.8)",
+					highlightFill: "rgba(220,220,220,0.75)",
+					highlightStroke: "rgba(220,220,220,1)",
+					data: [65, 59, 80, 81, 56, 55, 40]
+				},
+				{
+					label: "My Second dataset",
+					fillColor: "rgba(151,187,205,0.5)",
+					strokeColor: "rgba(151,187,205,0.8)",
+					highlightFill: "rgba(151,187,205,0.75)",
+					highlightStroke: "rgba(151,187,205,1)",
+					data: [28, 48, 40, 19, 86, 27, 90]
+				}
+			]
+	};
+	var lineData = {
+		labels: ["January", "February", "March", "April", "May", "June", "July"],
+		datasets: [
+			{
+				label: "My First dataset",
+				fillColor: "rgba(220,220,220,0.2)",
+				strokeColor: "rgba(220,220,220,1)",
+				pointColor: "rgba(220,220,220,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(220,220,220,1)",
+				data: [65, 59, 80, 81, 56, 55, 40]
+			},
+			{
+				label: "My Second dataset",
+				fillColor: "rgba(151,187,205,0.2)",
+				strokeColor: "rgba(151,187,205,1)",
+				pointColor: "rgba(151,187,205,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(151,187,205,1)",
+				data: [28, 48, 40, 19, 86, 27, 90]
+			}
+		]
+	};
 	$scope.newPoll = {chartType:"Pie",choices:[]};
 var context = document.getElementById('chart').getContext('2d');
-var skillsChart;
+var prevChart = new Chart(context).Pie(pieData);
 	
 	$scope.chartPreview = function(){
 
 		if($scope.newPoll.chartType==="Pie"){
-			skillsChart = new Chart(context).Pie(pieData);
+			prevChart.destroy();
+			prevChart = new Chart(context).Pie(pieData);
 		}
 		else if($scope.newPoll.chartType==="Bar"){
-			skillsChart = new Chart(context).Bar(barData);
+			prevChart.destroy();
+			prevChart = new Chart(context).Bar(barData);
 		}		
 		else if($scope.newPoll.chartType==="Line"){
-			skillsChart = new Chart(context).Line(lineData);
+			prevChart.destroy();
+			prevChart = new Chart(context).Line(lineData);
 		}
 	}
 
