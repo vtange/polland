fdd9f6ede6f9bf9da3d6c7c876d7848b2aec906d
Victor Tang <victortang1992@gmail.com>
2016-01-29 21:51:25 -0800

    remove mock mypolls

--- public/app.js
+++ public/app.js
@@ -11,28 +11,7 @@ app.controller('MainCtrl', ['$scope', 'UserService', function($scope, UserServic
     $scope.UserService = UserService; // load service
 	
     //$scope.otherPolls = [poll1, poll2]
-    $scope.myPolls = [{
-						question: 'Is Dart awesome?',
-						asker        : 'Ford',
-						postDate	: 'February 4, 2016',
-						link		: "F42af-493k",
-						choices: [
-						  { choice: 'Of course!', votes: 0 },
-						  { choice: 'Eh', votes: 0 },
-						  { choice: 'No. I like plain JS', votes: 0 }
-						]
-					  },
-					  {
-						question: 'Is it OK to eat ice cream when you are sick?',
-						asker        : 'Thomas',
-						postDate	: 'July 31, 2015',
-						link		: "43ase2-845m",
-						choices: [
-						  { choice: '100% no', votes: 0 },
-						  { choice: '200% no', votes: 0 },
-						  { choice: '300% no', votes: 0 }
-						]
-					  }]
+    $scope.myPolls = [];
 	
     $scope.activeUser = null;
 	$scope.init = function(package) {
