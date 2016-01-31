2e366c775b74f9749e69cb82afe6f44ece6347a7
Victor Tang <victortang1992@gmail.com>
2016-01-30 22:23:30 -0800

    initdb generates minimum 1 user and mock polls go to that user.

--- config/initDB.js
+++ config/initDB.js
@@ -1,22 +1,40 @@
 console.log("	CONFIG/INITDB.JS");
 
 // load up the poll model
+var User       = require('../app/models/user');
 var Poll       = require('../app/models/poll');
 var shortid  = require('shortid');
+var asyncc  = require('async');
 
 module.exports = function(){
+	function minimumUser(callback){
+			User.findOne(function(err, user){
+				if(err){
+					throw err;
+				}
+				if (user === null){
+					var mockUser=new User();
+					mockUser.save(function(err) {
+							if (err)
+								throw err;
+						console.log('saved a new user')
+					});
+				}
+				callback(null,mockUser);
+			})
+	}
+	function genMockPolls(user, callback){
 				Poll.findOne(function(err, poll){
 
 					if (poll === null) {
-						console.log('create sample polls');
+
 					// create sample polls
 					var samplePolls = [
 					  {
 						question: 'Is Meteor awesome?',
-						asker        : 'Anonymous',
+						asker        : user,
 						postDate	: 'December 31, 2015',
 						chartType	: 'Pie',
-						link	: shortid.generate(),
 						choices: [
 						  { choice: 'Of course!', votes: 0 },
 						  { choice: 'Eh', votes: 0 },
@@ -25,10 +43,9 @@ module.exports = function(){
 					  },
 					  {
 						question: 'Is CSS3 Flexbox the greatest thing since array_slice(bread)?',
-						asker        : 'Anonymous',
+						asker        : user,
 						postDate	: 'December 31, 2015',
 						chartType	: 'Pie',
-						link	: shortid.generate(),
 						choices: [
 						  { choice: 'No', votes: 0 },
 						  { choice: 'Dunno', votes: 0 },
@@ -45,15 +62,32 @@ module.exports = function(){
 						newPoll.question = samplePolls[i].question;
 						newPoll.asker = samplePolls[i].asker;
 						newPoll.postDate = samplePolls[i].postDate;
+						newPoll.link = shortid.generate();
 						newPoll.choices = samplePolls[i].choices;
 						newPoll.save(function(err) {
 							if (err)
 								throw err;
-							console.log('saved a new poll')
+							user.polls.push(newPoll);
+							user.save(function(err) {
+								if (err)
+									throw err;
+								console.log('new poll added to user')
+							});
 						});
+
 					};
 
 				  }
-					
+				  callback(null,"done");
 			  });
+	}
+	asyncc.waterfall([minimumUser,genMockPolls], function(err,result){
+		if(err){
+			throw err;
+		}
+		else{
+			return result;
+		}
+	})
+
 }
