846b448787f235153b1f3087428327d6eb8bc32b
Victor Tang <victortang1992@gmail.com>
2016-01-31 17:20:15 -0800

    get data and render poll. doesn't render polls with 0 votes

--- public/poll/chartviewer.js
+++ public/poll/chartviewer.js
@@ -3,13 +3,43 @@
   var app = angular.module('chartViewer', []);
 
 app.controller('MainCtrl', ['$scope', function($scope){
-
-	var context = document.getElementById('chart').getContext('2d');
+	function getRandomColor() {
+		var letters = '0123456789ABCDEF'.split('');
+		var color = '#';
+		for (var i = 0; i < 6; i++ ) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	}
 	$scope.poll = {};
 	$scope.init = function(package){
 		$scope.poll = package[0];
+		$scope.getData();
 	}
-	
+	$scope.getData = function(){
+		var data=[];
+		$scope.poll.choices.forEach(function(choice){
+			data.push({value:choice.votes,label:choice.choice,color:getRandomColor()})
+		})
+		console.log(data);
+		$scope.chartView(data);
+	};
+	var context = document.getElementById('chart').getContext('2d');
+	$scope.chartView = function(data){
+		var prevChart = new Chart(context).Pie(data); ;
+		if($scope.poll.chartType==="Pie"){
+			prevChart.destroy();
+			prevChart = new Chart(context).Pie(data);
+		}
+		else if($scope.poll.chartType==="Bar"){
+			prevChart.destroy();
+			prevChart = new Chart(context).Bar(data);
+		}
+		else if($scope.poll.chartType==="Line"){
+			prevChart.destroy();
+			prevChart = new Chart(context).Line(data);
+		}
+	}
 	
 	
 	
