6d58224d4209c47b2bed76a8c625029e4bbade85
Victor Tang <victortang1992@gmail.com>
2016-02-01 17:22:06 -0800

    sidescrolling action

--- public/app.js
+++ public/app.js
@@ -12,7 +12,31 @@ app.controller('MainCtrl', ['$scope', function($scope){
 		$scope.otherPolls = package[1][0];
 		$scope.myPolls = package[2][0];
 	}
-	
 }]);//end of controller
+	
+	
+	$(function() {
+		var myPollsPosition = 0;
+		var otherPollsPosition = 0;
+	   $(".user-ctrl-bar").mousewheel(function(event, delta) {
+		   if(myPollsPosition + delta*100<=0 && myPollsPosition + delta*100>= $(".content").outerWidth() - $("#myPolls").outerWidth()-100){
+			   myPollsPosition += (delta*100);
+			   $("#myPolls").css("transform","translateX("+myPollsPosition+"px)")
+			   console.log(myPollsPosition);
+			  event.preventDefault();
+		   }
+	   });
+	   $(".polls-viewport").mousewheel(function(event, delta) {
+		   if(otherPollsPosition + delta*100<=0 && otherPollsPosition + delta*100>= $(".content").outerWidth() - $("#otherPolls").outerWidth()-100){
+			   otherPollsPosition += (delta*100);
+			   $("#otherPolls").css("transform","translateX("+otherPollsPosition+"px)")
+			  event.preventDefault();
+		   }
+	   });
+	});
+	
+	
+	
+	
   //end of function
 })();
